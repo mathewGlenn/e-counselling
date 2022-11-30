de5c7c73292cf5dee62d49b3eb9182335d6abafb
JoshuaD10 <joshuadumaliang46@gmail.com>
2022-11-30 18:42:23 +0800

    finish login

---
+++ client/includes/loginverify.inc.php
@@ -0,0 +1,24 @@
+<?php
+    //start session
+    session_start();
+
+    $id = openssl_decrypt($_SESSION['users_id'], "AES-128-ECB", "password@2023");
+
+    $sql = "SELECT * FROM tblusers WHERE id='$id';";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+
+    if ($resultCheck > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $users_id = $row['id'];
+            $users_fullname = $row['users_fullname'];
+            $users_contact_email = $row['users_contact_email'];
+            $users_college = $row['users_college'];
+            $users_course = $row['users_course'];
+            $users_year = $row['users_year'];
+        }
+    }
+    else {
+        unset($_SESSION['users_id']);
+    }
+?>
