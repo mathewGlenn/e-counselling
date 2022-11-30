6f3a647973d863a1aae1180e62cef0abf7b1908c
JoshuaD10 <joshuadumaliang46@gmail.com>
2022-11-30 19:49:08 +0800

    finish update users

---
+++ client/includes/editprofile.inc.php
@@ -0,0 +1,34 @@
+<?php
+    if(isset($_POST['submit'])) {
+        include 'db.inc.php';
+        include 'loginverify.inc.php';
+
+        $fullname = $_POST['fullname'];
+        $contactEmail = $_POST['contactEmail'];
+        $college = $_POST['college'];
+        $course = $_POST['course'];
+        $year = $_POST['year'];
+
+        if(empty($fullname) || empty($contactEmail) || empty($college) || empty($course)) {
+            echo "empty";
+        }
+        else {
+            //created a template 
+            $sql = "UPDATE tblusers SET users_fullname=?, users_contact_email=?, users_college=?, users_course=?, users_year=? WHERE id=?;";
+            //create a prepared statement
+            $stmt = mysqli_stmt_init($conn);
+            //prepare the prepared statement
+            if(!mysqli_stmt_prepare($stmt, $sql)) {
+                echo "SQL statement failed";
+            }
+            else {
+                //bind parameters to the placeholder
+                mysqli_stmt_bind_param($stmt, "ssssss", $fullname, $contactEmail, $college, $course, $year, $users_id);
+                //run parameters inside database
+                mysqli_stmt_execute($stmt);
+
+                echo "success";
+            }
+        }
+    }
+?>
