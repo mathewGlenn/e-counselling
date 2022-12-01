93d686b0c490eae13a4e19440cde4d6fd746b723
JoshuaD10 <joshuadumaliang46@gmail.com>
2022-12-01 13:07:14 +0800

    finish login

---
+++ admin/includes/login.inc.php
@@ -0,0 +1,56 @@
+<?php
+    if(isset($_POST['submit'])) {
+        include 'db.inc.php';
+
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        if(empty($email) || empty($password)) {
+            echo "empty";
+        }
+        else {
+            //created a template 
+            $sql = "SELECT * FROM tblemployee WHERE employee_email=?;";
+            //create a prepared statement
+            $stmt = mysqli_stmt_init($conn);
+            //prepare the prepared statement
+            if(!mysqli_stmt_prepare($stmt, $sql)) {
+                echo "SQL statement failed";
+            }
+            else {
+                //bind parameters to the placeholder
+                mysqli_stmt_bind_param($stmt, "s", $email);
+                //run parameters inside database
+                mysqli_stmt_execute($stmt);
+                $result = mysqli_stmt_get_result($stmt);
+                //count result
+                $resultCheck = mysqli_num_rows($result);
+
+                if($resultCheck > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $id = $row['id'];
+                        $password = password_verify($password, $row['employee_password']);
+
+                        $plaintext = $id;
+                        $id = openssl_encrypt($plaintext, "AES-128-ECB", "password@2023");
+
+                        if($password == 1) {
+                            //start session
+                            session_start();
+                            //create session
+                            $_SESSION['employee_id'] = $id;
+        
+                            echo "success";
+                        }
+                        else {
+                            echo "password";
+                        }
+                    }
+                }
+                else {
+                    echo "email";
+                }
+            }
+        }
+    }
+?>
