83a90c28348a56179e291549f6931b01aa36738c
JoshuaD10 <joshuadumaliang46@gmail.com>
2022-12-01 13:07:30 +0800

    add login verfiy

--- admin/includes/loginverify.inc.php
+++ admin/includes/loginverify.inc.php
@@ -2,23 +2,18 @@
     //start session
     session_start();
 
-    $id = openssl_decrypt($_SESSION['users_id'], "AES-128-ECB", "password@2023");
+    $id = openssl_decrypt($_SESSION['employee_id'], "AES-128-ECB", "password@2023");
 
-    $sql = "SELECT * FROM tblusers WHERE id='$id';";
+    $sql = "SELECT * FROM tblemployee WHERE id='$id';";
     $result = mysqli_query($conn, $sql);
     $resultCheck = mysqli_num_rows($result);
 
     if ($resultCheck > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
-            $users_id = $row['id'];
-            $users_email = $row['users_email'];
-            $users_fullname = $row['users_fullname'];
-            $users_college = $row['users_college'];
-            $users_course = $row['users_course'];
-            $users_year = $row['users_year'];
+            $employee_id = $row['id'];
         }
     }
     else {
-        unset($_SESSION['users_id']);
+        unset($_SESSION['employee_id']);
     }
 ?>
